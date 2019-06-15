dc7c6e7c20741ebc9a511c482dc684e6266bc00b
Charles Burton <charles.d.burton@gmail.com>
2019-06-15 11:04:26 -0600

    initial commit

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log"
+	"flag"
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
+	"./messagebus"
+)
+
+var usageStr = `
+Usage: pismoker [options]
+Options:
+	-nh, --nats-host       <NATSHost>     Start the controller connecting to the defined NATS Streaming server
+`
+
+func usage() {
+	log.Fatalf(usageStr)
+}
+
+func main() {
+	var natsHost string
+	flag.StringVar(&natsHost, "nh", "", "Start the controller connecting to the defined NATS Streaming server")
+	flag.StringVar(&natsHost, "nats-host", "", "Start the controller connecting to the defined NATS Streaming server")
+	flag.Parse()
+	go messagebus.Connect(natsHost)
+	http.HandleFunc("/send", handleSend)
+	log.Fatal(http.ListenAndServe(":7777", nil))
+}
+
+func handleSend(rw http.ResponseWriter, req *http.Request) {
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println(body)
+	var msg messagebus.Message
+	err = json.Unmarshal(body, &msg)
+	if err != nil {
+		log.Println(err)
+	}
+	messagebus.Publish(&msg)
+}
